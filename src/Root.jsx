37885cc00a8d35c68dba61b26e86ac7debcc83b0
Abdullah Al Noman <130217084+AbdullahAlNoman20@users.noreply.github.com>
2026-04-01 22:57:59 +0600

    Nav Bar And Footer Design

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+
 import { Outlet } from 'react-router-dom';
+import Nav from './Components/Nav';
+import Footer from './Components/Footer';
 
 
 const Root = () => {
     return (
-        <div className='text-zinc-600 bg-white font-poppins '>
+        <div className='min-h-screen flex flex-col'>
+            <Nav></Nav>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
-    );
+    ); 
 };
 
 export default Root;
